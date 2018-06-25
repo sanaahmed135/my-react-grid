563d13893b9856341c6f3420fada0d4d30243ddb
Sana Saad <s.saad@solvin.com>
2018-06-25 15:34:47 +0200

    resizable not working

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import ReactDom from 'react-dom'
 import ReactDataGrid from 'react-data-grid'
 
 
@@ -11,12 +10,48 @@ class App extends Component {
     super(props, context);
     this.createRows();
     this._columns = [
-      { key: 'id', name: 'ID',resizable: true },
-      { key: 'title', name: 'Title',resizable: true },
-      { key: 'count', name: 'Count',resizable: true } ];
+      {
+        key: 'id',
+        name: 'ID'
+      },
+      {
+        key: 'task',
+        name: 'Title',
+        resizable:true
+      },
+      {
+        key: 'priority',
+        name: 'Priority',
+        resizable:true
+      },
+      {
+        key: 'issueType',
+        name: 'Issue Type',
+        resizable:true
+      },
+      {
+        key: 'complete',
+        name: '% Complete',
+        resizable:true
+      },
+      {
+        key: 'startDate',
+        name: 'Start Date',
+        resizable:true
+      },
+      {
+        key: 'completeDate',
+        name: 'Expected Complete',
+        resizable:true
+      }
+     ];
 
     this.state = null;
   }
+
+  getRandomDate = (start, end) => {
+    return new Date(start.getTime() + Math.random() * (end.getTime() - start.getTime())).toLocaleDateString();
+  };
 
     createRows = () => {
       let rows = [];
@@ -24,7 +59,12 @@ class App extends Component {
         rows.push({
           id: i,
           title: 'Title ' + i,
-          count: i * 1000
+          complete: Math.min(100, Math.round(Math.random() * 110)),
+          priority: ['Critical', 'High', 'Medium', 'Low'][Math.floor((Math.random() * 3) + 1)],
+          issueType: ['Bug', 'Improvement', 'Epic', 'Story'][Math.floor((Math.random() * 3) + 1)],
+          startDate: this.getRandomDate(new Date(2015, 3, 1), new Date()),
+          completeDate: this.getRandomDate(new Date(), new Date(2016, 0, 1))
+
         });
       }
   
@@ -41,7 +81,6 @@ class App extends Component {
         columns={this._columns}
         rowGetter={this.rowGetter}
         rowsCount={this._rows.length}
-        
         minHeight={500} />);
   }
 }
