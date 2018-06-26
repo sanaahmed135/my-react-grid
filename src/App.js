9f2c3641a28cb2a0186820f4d54828d8e485001f
Sana Saad <s.saad@solvin.com>
2018-06-26 15:42:30 +0200

    grid with gridselect filters sort editor

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import ReactDataGrid from 'react-data-grid'
+import ReactDataGrid,{Row} from 'react-data-grid'
 import '../node_modules/bootstrap/dist/css/bootstrap.css'
-import { Editors, Formatters} from 'react-data-grid-addons'
+import { Editors, Formatters,Selectors} from 'react-data-grid-addons'
 import update from 'immutability-helper';
 import PropTypes from 'prop-types';
 
@@ -42,6 +42,35 @@ class PercentCompleteFormatter extends Component {
       </div>);
   }
 }
+
+class RowRenderer extends React.Component {
+  static propTypes = {
+    idx: PropTypes.string.isRequired
+  };
+
+  setScrollLeft = (scrollBy) => {
+    // if you want freeze columns to work, you need to make sure you implement this as apass through
+    this.row.setScrollLeft(scrollBy);
+  };
+
+  getRowStyle = () => {
+    return {
+      color: this.getRowBackground()
+    };
+  };
+
+  getRowBackground = () => {
+    return this.props.idx % 2 ?  'green' : 'blue';
+  };
+
+  render() {
+    // here we are just changing the style
+    // but we could replace this with anything we liked, cards, images, etc
+    // usually though it will just be a matter of wrapping a div, and then calling back through to the grid
+    return (<div style={this.getRowStyle()}><Row ref={ node => this.row = node } {...this.props}/></div>);
+  }
+}
+
 class App extends Component {
   constructor(props, context) {
     super(props, context);
@@ -106,7 +135,7 @@ class App extends Component {
      ];
       let originalRows = this.createRows(1000);
       let rows = originalRows.slice(0);
-     this.state = { rows: this.createRows(1000) , filters: {}, sortColumn: null, sortDirection: null };
+     this.state = { rows: this.createRows(1000) , filters: {}, sortColumn: null, sortDirection: null, selectedIndexes: []  };
   }
 
   getRandomDate = (start, end) => {
@@ -131,11 +160,22 @@ class App extends Component {
   
       return rows;
     };
+    
+    getRows = () => {
+      return Selectors.getRows(this.state);
+    };
   
+    getSize = () => {
+      return this.getRows().length;
+    };
+
     rowGetter = (i) => {
       return this.state.rows[i];
     };
-  
+
+    onRowsSelected = (rows) => {
+      this.setState({selectedIndexes: this.state.selectedIndexes.concat(rows.map(r => r.rowIdx))});
+    };
     handleGridRowsUpdated = ({ fromRow, toRow, updated }) => {
       let rows = this.state.rows.slice();
   
@@ -162,6 +202,34 @@ class App extends Component {
   
       this.setState({ rows });
     };
+
+    // onRowClick = (rowIdx, row) => {
+    //   let rows = this.state.rows.slice();
+    //   rows[rowIdx] = Object.assign({}, row, {isSelected: !row.isSelected});
+    //   this.setState({ rows });
+    // };
+  
+    // onKeyDown = (e) => {
+    //   if (e.ctrlKey && e.keyCode === 65) {
+    //     e.preventDefault();
+  
+    //     let rows = [];
+    //     this.state.rows.forEach((r) =>{
+    //       rows.push(Object.assign({}, r, {isSelected: true}));
+    //     });
+  
+    //     this.setState({ rows });
+    //   }
+    // };
+
+    onRowsSelected = (rows) => {
+      this.setState({selectedIndexes: this.state.selectedIndexes.concat(rows.map(r => r.rowIdx))});
+    };
+  
+    onRowsDeselected = (rows) => {
+      let rowIndexes = rows.map(r => r.rowIdx);
+      this.setState({selectedIndexes: this.state.selectedIndexes.filter(i => rowIndexes.indexOf(i) === -1 )});
+    };
   render() {
     return (
       <ReactDataGrid
@@ -171,7 +239,19 @@ class App extends Component {
       rowsCount={this.state.rows.length}
       minHeight={500}
       onGridRowsUpdated={this.handleGridRowsUpdated}
-      onGridSort={this.handleGridSort} />);
+      onGridSort={this.handleGridSort}
+      rowRenderer={RowRenderer} 
+      onRowClick={this.onRowClick}
+      onGridKeyDown={this.onKeyDown}
+      rowSelection={{
+        showCheckbox: true,
+        enableShiftSelect: true,
+        onRowsSelected: this.onRowsSelected,
+        onRowsDeselected: this.onRowsDeselected,
+        selectBy: {
+          indexes: this.state.selectedIndexes
+        }
+      }}  />);
 
   }
 }
