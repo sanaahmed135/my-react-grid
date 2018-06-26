7092852c26dc38afa15bfbc46d5df91017a14334
Sana Saad <s.saad@solvin.com>
2018-06-26 11:56:20 +0200

    sort

--- src/App.js
+++ src/App.js
@@ -3,10 +3,45 @@ import logo from './logo.svg';
 import './App.css';
 import ReactDataGrid from 'react-data-grid'
 import '../node_modules/bootstrap/dist/css/bootstrap.css'
-import { Editors, Formatters } from 'react-data-grid-addons'
+import { Editors, Formatters} from 'react-data-grid-addons'
 import update from 'immutability-helper';
+import PropTypes from 'prop-types';
 
+const { AutoComplete: AutoCompleteEditor, DropDownEditor } = Editors;
+const { DropDownFormatter } = Formatters;
 
+// options for priorities autocomplete editor
+const priorities = [{ id: 0, title: 'Critical' }, { id: 1, title: 'High' }, { id: 2, title: 'Medium' }, { id: 3, title: 'Low'} ];
+const PrioritiesEditor = <AutoCompleteEditor options={priorities} />;
+
+// options for IssueType dropdown editor
+// these can either be an array of strings, or an object that matches the schema below.
+const issueTypes = [
+  { id: 'bug', value: 'bug', text: 'Bug', title: 'Bug' },
+  { id: 'improvement', value: 'improvement', text: 'Improvement', title: 'Improvement' },
+  { id: 'epic', value: 'epic', text: 'Epic', title: 'Epic' },
+  { id: 'story', value: 'story', text: 'Story', title: 'Story' }
+];
+const IssueTypesEditor = <DropDownEditor options={issueTypes}/>;
+
+const IssueTypesFormatter = <DropDownFormatter options={issueTypes} value="bug"/>;
+
+// Custom Formatter component
+class PercentCompleteFormatter extends Component {
+  static propTypes = {
+    value: PropTypes.number.isRequired
+  };
+  render() {
+    const percentComplete = this.props.value + '%';
+    return (
+      <div className="progress" style={{marginTop: '20px'}}>
+        <div className="progress-bar" role="progressbar" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100" style={{width: percentComplete}}>
+          {percentComplete}
+          
+        </div>
+      </div>);
+  }
+}
 class App extends Component {
   constructor(props, context) {
     super(props, context);
@@ -26,35 +61,40 @@ class App extends Component {
         key: 'priority',
         name: 'Priority',
         resizable:true,
+        editor: PrioritiesEditor,
         editable: true
       },
       {
         key: 'issueType',
         name: 'Issue Type',
         resizable:true,
+        editor: IssueTypesEditor,
         editable: true
       },
       {
         key: 'complete',
         name: '% Complete',
         resizable:true,
-        editable: true
+        formatter: PercentCompleteFormatter
       },
       {
         key: 'startDate',
         name: 'Start Date',
         resizable:true,
-        editable: true
+        editable: true,
+        sortable: true
       },
       {
         key: 'completeDate',
         name: 'Expected ',
         resizable:true,
-        editable: true
+        editable: true,
+        sortable: true
       }
      ];
-
-     this.state = { rows: this.createRows(1000) };
+     let originalRows = this.createRows(1000);
+     let rows = originalRows.slice(0);
+     this.state = { rows: this.createRows(2000) };
   }
 
   getRandomDate = (start, end) => {
@@ -62,7 +102,7 @@ class App extends Component {
   };
 
     createRows = () => {
-      
+
       let rows = [];
       for (let i = 1; i < 1000; i++) {
         rows.push({
@@ -97,6 +137,19 @@ class App extends Component {
     };
   
 
+    handleGridSort = (sortColumn, sortDirection) => {
+      const comparer = (a, b) => {
+        if (sortDirection === 'ASC') {
+          return (a[sortColumn] > b[sortColumn]) ? 1 : -1;
+        } else if (sortDirection === 'DESC') {
+          return (a[sortColumn] < b[sortColumn]) ? 1 : -1;
+        }
+      };
+  
+      const rows = sortDirection === 'NONE' ? this.state.originalRows.slice(0) : this.state.rows.sort(comparer);
+  
+      this.setState({ rows });
+    };
   render() {
     return (
       <ReactDataGrid
@@ -105,7 +158,8 @@ class App extends Component {
       rowGetter={this.rowGetter}
       rowsCount={this.state.rows.length}
       minHeight={500}
-      onGridRowsUpdated={this.handleGridRowsUpdated} />);
+      onGridRowsUpdated={this.handleGridRowsUpdated}
+      onGridSort={this.handleGridSort} />);
   }
 }
 
