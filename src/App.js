8011134b80502bf18aad08256c20b51525ebf545
Sana Saad <s.saad@solvin.com>
2018-06-27 15:07:27 +0200

    Cell selection event

--- src/App.js
+++ src/App.js
@@ -2,11 +2,17 @@ import React, { Component } from 'react';
 import './App.css';
 import ReactDataGrid,{Row} from 'react-data-grid'
 import '../node_modules/bootstrap/dist/css/bootstrap.css'
-import { Editors, Formatters} from 'react-data-grid-addons'
+import { Editors, Formatters,Toolbar} from 'react-data-grid-addons'
 import update from 'immutability-helper';
 import PropTypes from 'prop-types';
 
-const { Toolbar, Filters: { NumericFilter, AutoCompleteFilter, MultiSelectFilter, SingleSelectFilter }, Data: { Selectors } } = require('react-data-grid-addons');
+// //row reordering
+// const {
+//   Draggable: { Container: DraggableContainer, RowActionsCell, DropTargetRowContainer }
+// } = require('react-data-grid-addons');
+
+// const RowRendererReordering = DropTargetRowContainer(ReactDataGrid.Row);
+const { Filters: { NumericFilter, AutoCompleteFilter, MultiSelectFilter, SingleSelectFilter }, Data: { Selectors } } = require('react-data-grid-addons');
 const { AutoComplete: AutoCompleteEditor, DropDownEditor } = Editors;
 const { DropDownFormatter } = Formatters;
 
@@ -23,7 +29,6 @@ const issueTypes = [
   { id: 'story', value: 'story', text: 'Story', title: 'Story' }
 ];
 const IssueTypesEditor = <DropDownEditor options={issueTypes}/>;
-
 const IssueTypesFormatter = <DropDownFormatter options={issueTypes} value="bug"/>;
 
 // Custom Formatter component
@@ -43,15 +48,16 @@ class PercentCompleteFormatter extends Component {
   }
 }
 
+//to render alternate rows into different colors
 class RowRenderer extends React.Component {
   static propTypes = {
     idx: PropTypes.string.isRequired
   };
 
-  setScrollLeft = (scrollBy) => {
-    // if you want freeze columns to work, you need to make sure you implement this as apass through
-    this.row.setScrollLeft(scrollBy);
-  };
+//   setScrollLeft = (scrollBy) => {
+//     // if you want freeze columns to work, you need to make sure you implement this as apass through
+//     this.row.setScrollLeft(scrollBy);
+//   };
 
   getRowStyle = () => {
     return {
@@ -74,226 +80,230 @@ class RowRenderer extends React.Component {
 class App extends Component {
   constructor(props, context) {
     super(props, context);
-    // this.createRows();
     this._columns = [
       {
         key: 'id',
-        name: 'ID',       
-        locked:true,
+        name: 'ID',
+        width: 120,
         filterable: true,
         filterRenderer: NumericFilter
       },
       {
-        key: 'title',
+        key: 'task',
         name: 'Title',
-        editable: true,
-        resizable:true,
         filterable: true,
-        sortable: true
+        sortable: true,
+        editable: true
       },
       {
         key: 'priority',
         name: 'Priority',
-        resizable:true,
-        editor: PrioritiesEditor,
-        editable: true,
         filterable: true,
         filterRenderer: MultiSelectFilter,
-        sortable: true
+        sortable: true,
+        editable: true
       },
       {
         key: 'issueType',
         name: 'Issue Type',
-        resizable:true,
-        editor: IssueTypesEditor,
-        editable: true,
         filterable: true,
         filterRenderer: SingleSelectFilter,
-        sortable: true
+        sortable: true,
+        editable: true
       },
       {
         key: 'developer',
         name: 'Developer',
-        sortable:true,
-        editable: true,
         filterable: true,
-        filterRenderer: AutoCompleteFilter
+        filterRenderer: AutoCompleteFilter,
+        sortable: true,
+        editable: true
       },
       {
         key: 'complete',
         name: '% Complete',
-        resizable:true,
-        editable: true,
-        formatter: PercentCompleteFormatter,
         filterable: true,
         filterRenderer: NumericFilter,
-        sortable: true
+        sortable: true,
+        editable: true,
+        formatter: PercentCompleteFormatter
       },
       {
         key: 'startDate',
         name: 'Start Date',
-        resizable:true,
-        editable: true,
         filterable: true,
-        filterRenderer: NumericFilter,
-        sortable: true
+        sortable: true,
+        editable: true
       },
       {
         key: 'completeDate',
-        name: 'Expected ',
-        resizable:true,
-        editable: true,
+        name: 'Expected Complete',
         filterable: true,
-        filterRenderer: NumericFilter,
-        sortable: true
+        sortable: true,
+        editable: true
       }
-     ];
-      let originalRows = this.createRows(1000);
-      let rows = originalRows.slice(0);
-     this.state = { rows: this.createRows(1000) , filters: {}, sortColumn: null, sortDirection: null, selectedIndexes: []  };
+    ];
+
+    this.state = { rows: this.createRows(1000).slice(0), filters: {}, originalRows : this.createRows(1000) ,selectedIndexes: [], selectedIds: [1, 2],selectedRows: []};
   }
 
   getRandomDate = (start, end) => {
     return new Date(start.getTime() + Math.random() * (end.getTime() - start.getTime())).toLocaleDateString();
   };
 
-    createRows = () => {
+  createRows = (numberOfRows) => {
+    let rows = [];
+    for (let i = 1; i < numberOfRows; i++) {
+      rows.push({
+        id: i,
+        task: 'Task ' + i,
+        complete: Math.min(100, Math.round(Math.random() * 110)),
+        priority: ['Critical', 'High', 'Medium', 'Low'][Math.floor((Math.random() * 3) + 1)],
+        issueType: ['Bug', 'Improvement', 'Epic', 'Story'][Math.floor((Math.random() * 3) + 1)],
+        developer: ['James', 'Tim', 'Daniel', 'Alan'][Math.floor((Math.random() * 3) + 1)],
+        startDate: this.getRandomDate(new Date(2015, 3, 1), new Date()),
+        completeDate: this.getRandomDate(new Date(), new Date(2016, 0, 1))
+      });
+    }
+    return rows;
+  };
 
-      let rows = [];
-      for (let i = 1; i < 1000; i++) {
-        rows.push({
-          id: i,
-          title: 'Title ' + i,
-          complete: Math.min(100, Math.round(Math.random() * 110)),
-          priority: ['Critical', 'High', 'Medium', 'Low'][Math.floor((Math.random() * 3) + 1)],
-          issueType: ['Bug', 'Improvement', 'Epic', 'Story'][Math.floor((Math.random() * 3) + 1)],
-          developer: ['James', 'Tim', 'Daniel', 'Alan'][Math.floor((Math.random() * 3) + 1)],
-          startDate: this.getRandomDate(new Date(2015, 3, 1), new Date()),
-          completeDate: this.getRandomDate(new Date(), new Date(2016, 0, 1))
-
-        });
+  handleGridSort = (sortColumn, sortDirection) => {
+    const comparer = (a, b) => {
+      if (sortDirection === 'ASC') {
+        return (a[sortColumn] > b[sortColumn]) ? 1 : -1;
+      } else if (sortDirection === 'DESC') {
+        return (a[sortColumn] < b[sortColumn]) ? 1 : -1;
       }
-  
-      return rows;
-    };
-    
-    getRows = () => {
-      return Selectors.getRows(this.state);
-    };
-  
-    getSize = () => {
-      return this.getRows().length;
     };
 
-    rowGetter = (i) => {
-      return this.state.rows[i];
-    };
+    const rows = sortDirection === 'NONE' ? this.state.originalRows.slice(0) : this.state.rows.sort(comparer);
 
-    onRowsSelected = (rows) => {
-      this.setState({selectedIndexes: this.state.selectedIndexes.concat(rows.map(r => r.rowIdx))});
-    };
-    handleGridRowsUpdated = ({ fromRow, toRow, updated }) => {
-      let rows = this.state.rows.slice();
-  
-      for (let i = fromRow; i <= toRow; i++) {
-        let rowToUpdate = rows[i];
-        let updatedRow = update(rowToUpdate, {$merge: updated});
-        rows[i] = updatedRow;
-      }
-  
-      this.setState({ rows });
-    };
-  
+    this.setState({ rows });
+  };
 
-    handleGridSort = (sortColumn, sortDirection) => {
-      const comparer = (a, b) => {
-        if (sortDirection === 'ASC') {
-          return (a[sortColumn] > b[sortColumn]) ? 1 : -1;
-        } else if (sortDirection === 'DESC') {
-          return (a[sortColumn] < b[sortColumn]) ? 1 : -1;
-        }
-      };
-  
-      const rows = sortDirection === 'NONE' ? this.state.originalRows.slice(0) : this.state.rows.sort(comparer);
-  
-      this.setState({ rows });
-    };
+  rowGetter = (index) => {
+    return Selectors.getRows(this.state)[index];
+  };
 
-    // onRowClick = (rowIdx, row) => {
-    //   let rows = this.state.rows.slice();
-    //   rows[rowIdx] = Object.assign({}, row, {isSelected: !row.isSelected});
-    //   this.setState({ rows });
-    // };
-  
-    // onKeyDown = (e) => {
-    //   if (e.ctrlKey && e.keyCode === 65) {
-    //     e.preventDefault();
-  
-    //     let rows = [];
-    //     this.state.rows.forEach((r) =>{
-    //       rows.push(Object.assign({}, r, {isSelected: true}));
-    //     });
-  
-    //     this.setState({ rows });
-    //   }
-    // };
+  rowsCount = () => {
+    return Selectors.getRows(this.state).length;
+  };
 
-    onRowsSelected = (rows) => {
-      this.setState({selectedIndexes: this.state.selectedIndexes.concat(rows.map(r => r.rowIdx))});
-    };
-  
-    onRowsDeselected = (rows) => {
-      let rowIndexes = rows.map(r => r.rowIdx);
-      this.setState({selectedIndexes: this.state.selectedIndexes.filter(i => rowIndexes.indexOf(i) === -1 )});
-    };
+  // //row reordering
+  // isDraggedRowSelected = (selectedRows, rowDragSource) => {
+  //   if (selectedRows && selectedRows.length > 0) {
+  //     let key = this.props.rowIdx;
+  //     return selectedRows.filter(r => r[key] === rowDragSource.data[key]).length > 0;
+  //   }
+  //   return false;
+  // };
 
-    handleFilterChange = (filter) => {
-      let newFilters = Object.assign({}, this.state.filters);
-      if (filter.filterTerm) {
-        newFilters[filter.column.key] = filter;
-      } else {
-        delete newFilters[filter.column.key];
-      }
-      this.setState({ filters: newFilters });
-    };
-    
-    getValidFilterValues = (columnId) => {
-      let values = this.state.rows.map(r => r[columnId]);
-      return values.filter((item, i, a) => { return i === a.indexOf(item); });
-    };
-  
-    handleOnClearFilters = () => {
-      this.setState({ filters: {} });
-    };
-  
+  // //row reordering
+  // reorderRows = (e) => {
+  //   let selectedRows = Selectors.getSelectedRowsByKey({rowIdx: this.props.rowIdx, selectedKeys: this.state.selectedIds, rows: this.state.rows});
+  //   let draggedRows = this.isDraggedRowSelected(selectedRows, e.rowSource) ? selectedRows : [e.rowSource.data];
+  //   let undraggedRows = this.state.rows.filter(function(r) {
+  //     return draggedRows.indexOf(r) === -1;
+  //   });
+  //   let args = [e.rowTarget.idx, 0].concat(draggedRows);
+  //   Array.prototype.splice.apply(undraggedRows, args);
+  //   this.setState({rows: undraggedRows});
+  // };
+
+  //to select row
+  onRowsSelected = (rows) => {
+    this.setState({selectedIndexes: this.state.selectedIndexes.concat(rows.map(r => r.rowIdx))});
+  };
+  //to deselect a row
+  onRowsDeselected = (rows) => {
+    let rowIndexes = rows.map(r => r.rowIdx);
+    this.setState({selectedIndexes: this.state.selectedIndexes.filter(i => rowIndexes.indexOf(i) === -1 )});
+  };
+
+  // //to display selected cell number
+   onRowSelect = (rows) => {
+    this.setState({ selectedRows: rows });
+  };
+  //to display selected cell number
+  onCellSelected = ({ rowIdx, idx }) => {
+    this.grid.openCellEditor(rowIdx, idx);
+  };
+  //to display selected cell number
+  onCellDeSelected = ({ rowIdx, idx }) => {
+    if (idx === 2) {
+      alert('the editor for cell (' + rowIdx + ',' + idx + ') should have just closed');
+    }
+  };
+
+  //cell-drag-down(with editable property in columns set to true)
+  handleGridRowsUpdated = ({ fromRow, toRow, updated }) => {
+    let rows = this.state.rows.slice();
+
+    for (let i = fromRow; i <= toRow; i++) {
+      let rowToUpdate = rows[i];
+      let updatedRow = update(rowToUpdate, {$merge: updated});
+      rows[i] = updatedRow;
+    }
+
+    this.setState({ rows });
+  };
+
+  handleFilterChange = (filter) => {
+    let newFilters = Object.assign({}, this.state.filters);
+    if (filter.filterTerm) {
+      newFilters[filter.column.key] = filter;
+    } else {
+      delete newFilters[filter.column.key];
+    }
+    this.setState({ filters: newFilters });
+  };
+
+  getValidFilterValues = (columnId) => {
+    let values = this.state.rows.map(r => r[columnId]);
+    return values.filter((item, i, a) => { return i === a.indexOf(item); });
+  };
+
+  handleOnClearFilters = () => {
+    this.setState({ filters: {} });
+  };
+
   render() {
-    return (
+    const rowText = this.state.selectedRows.length === 1 ? 'row' : 'rows';
+    return  (
+      <div>
+      <span>{this.state.selectedRows.length} {rowText} selected</span>
       <ReactDataGrid
-      enableCellSelect={true}
-      columns={this._columns}
-      rowGetter={this.rowGetter}
-      rowsCount={this.state.rows.length}
-      minHeight={500}
-      onGridRowsUpdated={this.handleGridRowsUpdated}
-      onGridSort={this.handleGridSort}
-      rowRenderer={RowRenderer} 
-      onRowClick={this.onRowClick}
-      onGridKeyDown={this.onKeyDown}
-      rowSelection={{
-        showCheckbox: true,
-        enableShiftSelect: true,
-        onRowsSelected: this.onRowsSelected,
-        onRowsDeselected: this.onRowsDeselected,
-        selectBy: {
-          indexes: this.state.selectedIndexes
-        }
-      }} 
-      toolbar={<Toolbar enableFilter={true}/>}
-      onAddFilter={this.handleFilterChange}
-      getValidFilterValues={this.getValidFilterValues}
-      onClearFilters={this.handleOnClearFilters} />);
-
+        ref={ node => this.grid = node }
+        enableCellSelect={true}
+        enableRowSelect="multi"
+        onGridSort={this.handleGridSort}
+        columns={this._columns}
+        rowGetter={this.rowGetter}
+        rowsCount={this.rowsCount()}
+        minHeight={500}
+        onRowSelect={this.onRowSelect}
+        onCellSelected={this.onCellSelected}
+        onCellDeSelected={this.onCellDeSelected}
+        rowSelection={{
+          showCheckbox: true,
+          enableShiftSelect: true,
+          onRowsSelected: this.onRowsSelected,
+          onRowsDeselected: this.onRowsDeselected,
+          selectBy: {
+            indexes: this.state.selectedIndexes,
+            keys: {rowIdx: this.props.rowIdx, values: this.state.selectedIds}
+          }
+        }}
+        rowRenderer={RowRenderer} 
+        toolbar={<Toolbar enableFilter={true}/>}
+        onAddFilter={this.handleFilterChange}
+        onGridRowsUpdated={this.handleGridRowsUpdated}
+        getValidFilterValues={this.getValidFilterValues}
+        onClearFilters={this.handleOnClearFilters} />
+        </div>);
   }
 }
+
 
 export default App;
